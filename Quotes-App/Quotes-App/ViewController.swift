b6845bc9bd8a379a98f8fd7fa9aafcc519a18b55
DaZenMesa <mehufarm@gmail.com>
2020-01-07 08:56:01 -0800

    Basic

--- Quotes-App/Quotes-App/ViewController.swift
+++ Quotes-App/Quotes-App/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var butStudentWork: UIButton!
+    @IBOutlet weak var butPathways: UIButton!
+    @IBOutlet weak var butFieldTrips: UIButton!
+    @IBOutlet weak var donationLink: UILabel!
+    var donationLinkContent:String = "This is a Placeholder Donation Link"
+    var sudentWork:String = "This is the Student Wrok Place Holder"
+    var pathways:String = "This is the Pathways Place Holder"
+    var fieldTrips:String = "This is the Field Trips Place Holder"
+    @IBOutlet weak var MainContent: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        donationLink.text = donationLinkContent
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func MasterButton(_ sender: UIButton) {
+        if sender == butStudentWork{
+            MainContent.text = sudentWork
+        }
+        else{
+            if sender == butPathways{
+                MainContent.text = pathways
+            }
+            else{
+                if sender == butFieldTrips{
+                    MainContent.text = fieldTrips
+                }
+            }
+        }
+    }
+    
 
 }
 
